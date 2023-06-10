73e544c5ef1ccbb5f25d1f4c824d23052f23bea0
mitunda <peterbrian28@gmail.com>
2023-06-10 13:21:43 +0300

    started styling

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById('submitButton').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
+
+//     console.log('Email:', email);
+//     console.log('Phone:', phone);
+// });
